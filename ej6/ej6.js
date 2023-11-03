b033b0cde9e10171392678e30870301e3812b2d4
Fernanda Maro <fernanda.maro.mo@gmail.com>
2023-11-02 23:05:42 -0600

    ejercicio listo

--- ej6/ej6.js
+++ ej6/ej6.js
@@ -2,3 +2,11 @@ Exercise # 6
 Write a function that performs the bubble algorithm.
 Input [3, 6, 12, 5, 100, 1]
 Output [1, 3, 5, 6, 12, 100]
+
+function compara( a, b){
+    return a - b;
+}
+
+const input = [3, 6, 12, 5, 100, 1];
+//aseguramos que sea el output correcto:0
+console.log(input.sort( compara ));
