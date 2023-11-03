e6e8ea88042efde92fa2a251cb0976418773533a
Fernanda Maro <fernanda.maro.mo@gmail.com>
2023-11-02 23:22:06 -0600

    ejercicio listo

--- ej3/ej3.js
+++ ej3/ej3.js
@@ -2,20 +2,17 @@ Exercise #3 (sugerencia reduce())
 Write a program to compute the sum and product (multiplication) of an array of numbers. Print out the sum and the product.
 Example: Given an array [1, 2, 3, 4] The sum is 10. The product is 24.
 
-function resultado(numbers) {
-    let sum = 0;
+const numeros = [1, 2, 3, 4];
+function resultado(numeros) {
+    let suma = 0;
     let product = 1;
   
-    for (let i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-      product *= numbers[i];
+    for (let i = 0; i < numeros.length; i++) {
+      suma += numeros[i];
+      product *= numeros[i];
     }
   
-    console.log(`The sum is ${sum}.`);
-    console.log(`The product is ${product}.`);
+    console.log(`La suma es ${suma}.`);
+    console.log(`La multiplicacion (producto) es ${product}.`);
   }
-  
-  // Example usage:
-  const numbers = [1, 2, 3, 4];
-  resultado(numbers); // Output will be "The sum is 10." and "The product is 24."
-  
+  resultado(numeros);
--- ej6/ej6.js
+++ ej6/ej6.js
@@ -8,5 +8,21 @@ function compara( a, b){
 }
 
 const input = [3, 6, 12, 5, 100, 1];
-//aseguramos que sea el output correcto:0
 console.log(input.sort( compara ));
+//↑ese es el que se me ocurrio ocupar pero investigando tambien puede ser este
+function bubbleSort(items) {
+    let length = items.length;
+    for (let i = 0; i < length; i++) {
+      for ( let i = 0; i < (length - i - 1); i++) {
+        if (items[i] > items[i + 1]) {
+          let a = items[i];
+          items[i] = items[i + 1];
+          items[i + 1] = a;
+        }
+      }
+    }
+  }
+  
+  let array = [3, 6, 12, 5, 100, 1];
+  bubbleSort(array);
+  console.log(array);
